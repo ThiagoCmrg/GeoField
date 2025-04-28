93fd72df22c6346d6cee176add115867971e76f1
ThiagoCmrg <thiagosantoscamargo@hotmail.com>
2025-04-28 15:19:44 -0300

    Imports das bibliotecas e tabelas com dados

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("a")
+import pandas as pd
+import folium
+from folium.plugins import MarkerCluster
+
+tabela_cidades= pd.read_excel('Caminho_Arquivo.xlsx')  ## Substitua pelo caminho do seu arquivo
+tabela_tecnicos= pd.read_excel('Caminho_Arquivo.xlsx')   ## Substitua pelo caminho do seu arquivo
+
+print(tabela_cidades)
+print(tabela_tecnicos)
